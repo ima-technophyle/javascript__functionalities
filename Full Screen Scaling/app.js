bfbdf38d107852daef544863871ec78408616ac3
Sivasanker NP <siva@Sivasankers-MacBook-Pro.local>
2022-01-22 22:03:32 +0530

    Full Screen Scaling:- Add two teddy

--- Full Screen Scaling/app.js
+++ Full Screen Scaling/app.js
@@ -1,13 +1,20 @@
 const dot = document.querySelector(".dot");
 const teddy = document.querySelector(".teddy");
+const teddyLarge = document.querySelector(".teddy-large");
 
 dot.addEventListener("click", (e) => handleEnlarge(e));
 
-function handleEnlarge(e){
-    const element = e.srcElement;
-    if(element === teddy){
-        dot.classList.toggle("enlarge")
-        return;
-    }
-    dot.classList.add("enlarge")
+function handleEnlarge(e) {
+  const element = e.srcElement;
+
+  if (element === teddyLarge) {
+    console.log("ee");
+    dot.classList.remove("enlarge");
+    teddy.classList.remove("teddy-disappear");
+    return;
+  }
+
+  if (element.className.includes("enlarge")) return;
+  dot.classList.add("enlarge");
+  teddy.classList.add("teddy-disappear");
 }
